147b837f7610be5559162348cdb1f4d12cfd2acc
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-26 18:31:22 -0500

    Fill out footer content.

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/Logo'
 import styles from './Footer.module.css'
+import FacebookIcon from '../../assets/FacebookIcon'
+import TwitterIcon from '../../assets/TwitterIcon'
+import InstagramIcon from '../../assets/InstagramIcon'
 
 export default function Footer() {
     return (
@@ -39,6 +42,32 @@ export default function Footer() {
                 </Link>
 
             </ul>
+
+            <p>
+                Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a week.
+            </p>
+
+            <p>
+                Copyright 2021. All Rights Reserved.
+            </p>
+
+            <div
+                className={styles.socials}
+            >
+
+                <button>
+                    <FacebookIcon />
+                </button>
+
+                <button>
+                    <TwitterIcon />
+                </button>
+
+                <button>
+                    <InstagramIcon />
+                </button>
+
+            </div>
 
         </footer>
     )
