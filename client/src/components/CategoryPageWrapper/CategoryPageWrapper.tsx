f7e09c6a8bc5d824333129e05c26abb76a8c40d4
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-28 16:17:11 -0500

    Add category header to product page wrapper.

--- client/src/components/CategoryPageWrapper/CategoryPageWrapper.tsx
+++ client/src/components/CategoryPageWrapper/CategoryPageWrapper.tsx
@@ -1,10 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import AboutCard from "../AboutCard/AboutCard";
 import Nav from "../Nav/Nav";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 export default function CategoryPageWrapper() {
+    const location = useLocation()
+
+    const isSmallScreen = useMediaQuery('(max-width: 480px)')
+
+    const localStyles = {
+        div: {
+            display: 'grid',
+            placeItems: 'center',
+            height: 102,
+            backgroundColor: 'var(--clr-black-900)'
+        },
+        h2: {
+            font: 'var(--font-product-head)',
+            color: 'var(--clr-white-900)',
+            letterSpacing: isSmallScreen ? '2px' : 'var(--space-product-head)'
+        }
+    }
+
     return (
         <main>
+
+            {
+                (location.pathname === '/headphones' ||
+                location.pathname === '/speakers' ||
+                location.pathname === '/earphones') && (
+                    <div style={localStyles.div}>
+                        <h2 style={localStyles.h2}>
+                            {location.pathname.replace('/', '').toUpperCase()}
+                        </h2>
+                    </div>
+                )
+            }
 
             <Outlet />
 
