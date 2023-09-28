0a3c272d9223fef2cecc78cc268ffe1245a08faf
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-28 15:55:13 -0500

    Add placeholder images for headphones category with rudimentary product display.

--- client/src/routes/Headphones/Headphones.tsx
+++ client/src/routes/Headphones/Headphones.tsx
@@ -1,10 +1,32 @@
-import CategorySection from "../../components/CategorySection/CategorySection";
+import CategorySection from "../../components/CategorySection/CategorySection"
+
+const PLACEHOLDER_HEADPHONES = [
+    {
+        id: '1',
+        name: 'X99 Mark II',
+        preview_image: '/x99-mark-two-category-preview'
+    },
+    {
+        id: '2',
+        name: 'X99 Mark I',
+        preview_image: '/x99-mark-one-category-preview'
+    },
+    {
+        id: '3',
+        name: 'X59',
+        preview_image: '/x59-category-preview'
+    }
+]
 
 export default function Headphones() {
     return (
         <CategorySection>
             
-            Headphones
+            {PLACEHOLDER_HEADPHONES.map(item => (
+                <p key={item.id}>
+                    {item.name}
+                </p>
+            ))}
 
         </CategorySection>
     )
