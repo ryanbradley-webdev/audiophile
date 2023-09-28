5c9abc928f0402e29d01181eb62786099d7efab8
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-28 16:44:44 -0500

    Expand product type definition and add sample data.

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -3,7 +3,14 @@ type Path = 'headphones' | 'speakers' | 'earphones'
 type Product = {
     id: string
     name: string
+    slug: string
     category: Path
+    image: string
     preview_img: string
     description: string
+    features: string
+    new: boolean
+    price: number
+    includes: { quantity: number, item: string }[]
+    gallery: []
 }
