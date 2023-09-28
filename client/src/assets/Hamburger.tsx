f874e7e00594ad9314c9ca1fb71093ab894e1687
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-27 19:33:55 -0500

    Update hamburger component to return a button with svg content and to accept button props. Add nav component to header with custom styling and toggle function hooked into hamburger component.

--- client/src/assets/Hamburger.tsx
+++ client/src/assets/Hamburger.tsx
@@ -1,9 +1,24 @@
-export default function Hamburger() {
+import { HTMLAttributes } from "react"
+
+export default function Hamburger({
+    ...props
+}: HTMLAttributes<HTMLButtonElement>) {
     return (
-        <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
-            <g fill="#FFF" fillRule="evenodd">
-                <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z"/>
-            </g>
-        </svg>
+        <button
+            onClick={props.onClick}
+            {...props}
+            style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                height: 'fit-content',
+                width: 'fit-content'
+            }}
+        >
+            <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg">
+                <g fill="#FFF" fillRule="evenodd">
+                    <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z"/>
+                </g>
+            </svg>
+        </button>
     )
 }
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import CartIcon from '../../assets/CartIcon'
 import Hamburger from '../../assets/Hamburger'
 import Logo from '../../assets/Logo'
+import Nav from '../Nav/Nav'
 import styles from './Header.module.css'
 
 export default function Header() {
+    const [menuVisible, setMenuVisible] = useState(false)
+
+    const handleToggleMenu = () => {
+        setMenuVisible(!menuVisible)
+    }
+
     return (
         <header
             className={styles.header}
         >
 
-            <Hamburger />
+            <Hamburger
+                onClick={handleToggleMenu}
+            />
+
+            {menuVisible && <Nav />}
 
             <Logo />
 
