eb9d3ab57aeb3bc728e7724d93fe34acfc8d001c
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-28 10:58:18 -0500

    Add placeholder content to featured product section on homepage.

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 import Hero from '../../components/Hero/Hero'
 import Nav from '../../components/Nav/Nav'
+import ZX9 from './images/image-speaker-zx9.png'
 import styles from './Home.module.css'
 
 export default function Home() {
@@ -14,28 +15,43 @@ export default function Home() {
 
             <Nav />
 
-            <section>
+            <section
+                className={styles.products}
+            >
 
-                <h2>
-                    ZX9<br />SPEAKER
-                </h2>
-
-                <p>
-                    Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
-                </p>
-
-                <Button
-                    variant='solid'
-                    color='black'
+                <div
+                    className={styles.featured_product}
                 >
 
-                    <Link
-                        to='/speakers/zx9'
-                    >
-                        SEE PRODUCT
-                    </Link>
+                    <img
+                        src={ZX9}
+                        alt='zx9'
+                        width='50%'
+                        height='auto'
+                    />
 
-                </Button>
+                    <h2>
+                        ZX9<br />SPEAKER
+                    </h2>
+
+                    <p>
+                        Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+                    </p>
+
+                    <Button
+                        variant='solid'
+                        color='black'
+                    >
+
+                        <Link
+                            to='/speakers/zx9'
+                        >
+                            SEE PRODUCT
+                        </Link>
+
+                    </Button>
+
+                </div>
 
             </section>
 
