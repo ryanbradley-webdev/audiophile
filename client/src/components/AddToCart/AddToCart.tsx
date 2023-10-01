f73338c7b5af230f0bb4a932656a682955565553
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-01 13:47:07 -0500

    Add state management to AddToCart component to handle incrementing and decrementing quantity.

--- client/src/components/AddToCart/AddToCart.tsx
+++ client/src/components/AddToCart/AddToCart.tsx
@@ -1,37 +1,63 @@
+import { useState } from 'react'
 import Button from '../Button/Button'
 import styles from './AddToCart.module.css'
 
 export default function AddToCart() {
-  return (
-    <div
-        className={styles.container}
-    >
+    const [quantity, setQuantity] = useState(1)
 
+    const handleIncrementQuantity = () => {
+        setQuantity(prev => prev + 1)
+    }
+
+    const handleDecrementQuantity = () => {
+        setQuantity(prev => {
+            if (prev > 1) {
+                return prev - 1
+            }
+
+            return prev
+        })
+    }
+
+    const handleAddToCart = () => {
+        // TODO add logic for adding product to cart
+    }
+
+    return (
         <div
-            className={styles.quantity}
+            className={styles.container}
         >
 
-            <button>
-                -
-            </button>
+            <div
+                className={styles.quantity}
+            >
 
-            <span>
-                1
-            </span>
+                <button
+                    onClick={handleDecrementQuantity}
+                >
+                    -
+                </button>
 
-            <button>
-                +
-            </button>
+                <span>
+                    {quantity}
+                </span>
+
+                <button
+                    onClick={handleIncrementQuantity}
+                >
+                    +
+                </button>
+
+            </div>
+
+            <Button
+                variant='solid'
+                color='beige'
+                onClick={handleAddToCart}
+            >
+                ADD TO CART
+            </Button>
 
         </div>
-
-        <Button
-            variant='solid'
-            color='beige'
-        >
-            ADD TO CART
-        </Button>
-
-    </div>
-  )
+    )
 }
