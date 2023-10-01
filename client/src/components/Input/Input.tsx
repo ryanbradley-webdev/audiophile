1c10bca76c5bcbee9a5bb3744efbd5997e51d04e
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-01 14:38:54 -0500

    Begin basic input component construction.

---
+++ client/src/components/Input/Input.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import styles from './Input.module.css'
+
+type InputProps = {
+    value: string
+    onChange: (arg: string) => void
+} & (TextProps | RadioProps)
+
+type TextProps = {
+    type: 'text' | 'email' | 'number'
+    label: string
+    htmlFor: string
+    placeholder: string
+}
+
+type RadioProps = {
+    type: 'radio'
+    options: string[]
+}
+
+export default function Input(props: InputProps) {
+    const { value, onChange, type } = props
+    
+    const [valid, setValid] = useState(true)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValid(e.target.value !== '')
+
+        onChange(e.target.value)
+    }
+
+    const content = type !== 'radio' ? (
+        <label
+            htmlFor={props.htmlFor}
+            className={styles.label}
+            aria-invalid={!valid}
+        >
+
+            <span>
+                {props.label}
+            </span>
+
+            <input
+                type={type}
+                id={props.htmlFor}
+                value={value}
+                onChange={handleChange}
+                placeholder={props.placeholder}
+            />
+
+        </label>
+    ) : (
+        props.options.map(option => (
+            <label>
+
+                <input
+                    type="radio"
+                    value={option}
+                    checked={option === value}
+                />
+
+                <span>
+                    {option}
+                </span>
+
+            </label>
+        ))
+    )
+
+    return (
+        <>
+            {content}
+        </>
+    )
+}
--- client/src/routes/Checkout/Checkout.tsx
+++ client/src/routes/Checkout/Checkout.tsx
@@ -1,9 +1,21 @@
+import Input from '../../components/Input/Input'
 import styles from './Checkout.module.css'
 
 export default function Checkout() {
   return (
-    <main>
-        Checkout
+    <main
+      className={styles.main}
+    >
+      
+      <Input
+        type='text'
+        label='hello'
+        htmlFor='hello'
+        value=''
+        placeholder='world'
+        onChange={(arg) => console.log(arg)}
+      />
+
     </main>
   )
 }
